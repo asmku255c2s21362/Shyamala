2d6fb3954a1f8bf8a204a76b6acc0f85ce1bac69
asmku255c2s21362 <146502687+asmku255c2s21362@users.noreply.github.com>
2023-10-12 18:28:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-write a program that determines whether a year entered by the user in a leap year or not using if elif
-#leap year
 
-"""
-year%4==0&
-year%100!=0/
-year%400==0
-"""
-def is leap year (year):
-if(year%4==0 and year%100!=0) or year%400==0:
-  return True
-else:
-  return False
 
-year=int(input("enter a year"))
 
-if is leap year (year):
-print ("{} is a leap year".format (year))
-else
-print("{} is not a leap year.".format(year))
+
+
+
+
+class student:
+    def__init__(self,name,roll_number,CGPA):
+self.name=Name
+self.roll number=roll_number
+self.cgpa=cgpa
+def sort_students(student_lists):
+  #sort the list of students in des order of cgpa
+  sorted_students=sorted (student lists,key=lambda student: student,cgpa,reverser=true)
+  #syntax=lambda arg.exp
+  return sorted_students#eg usage
+students=[
+        student("hari","a123",7.8),
+      student("srikanth","a124",8.9)
+      student ("Sowmya","a124",9.9)]
+sorted_students=sort_students (students)
+#print the sorted list of students
+for student in sorted_students:
+  print ("name={},roll no:{}, CGPA:{},format (student.name, student.roll no, student.cgpa"))
